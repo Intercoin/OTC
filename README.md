4006b9157304b6c8984af73a5e58b09168e511d4
SolidityNinja <SolidityNinja@yandex.ru>
2022-02-08 12:15:14 +0700

    functions signatures added

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # OTC
 Over-the-counter trades, that could involve multiple parties, swaps and chains.
+
+
+Wiki: https://issues.intercoin.org/projects/nugenesis/wiki/Introduction
+Issues: https://issues.intercoin.org/projects/nugenesis/issues
--- contracts/TradeRegistrator.sol
+++ contracts/TradeRegistrator.sol
@@ -1,5 +1,21 @@
 pragma solidity ^0.8.11;
 
 contract TradeRegistrator {
-    
+    function post(
+        bytes32 _tradeHash, 
+        uint256 _amount, 
+        address _tokenAddress, 
+        address _receiverAddress, 
+        uint256 _penaltyAmount
+    ) public {
+
+    }
+
+    function claim(bytes32 _tradeHash, bytes32 _signature) public {
+
+    }
+
+    function repost(bytes32 _tradeHash, bytes32[] memory _signatures) public {
+        
+    }
 }
